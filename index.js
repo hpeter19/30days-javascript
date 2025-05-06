03f8d2ec42a829909f44ebe0b57e4ab33bbff499
hpeter19 <njoshpitar@gmail.com>
2025-05-06 19:09:41 +0300

    length of a string

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const bigmouth = 'I\'ve got no right to take my place…';
 console.log(bigmouth);
 //inputing escape characters to ensure that the quotes are part of the text
+const browserType = "mozilla";
+browserType.length;
+//FINDING THE LENGTH OF A STRING 
