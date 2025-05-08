6684f7b76814cbd260ff2b68c4d9f19b27d0db4b
hpeter19 <njoshpitar@gmail.com>
2025-05-08 12:23:44 +0300

    performing addition on the variable

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -3,5 +3,10 @@ let radius = 10;
 const pi= 3.14;
 
 //let results = radius % 3;
-let results = pi * radius**2
-console.log(results);
+//let results = pi * radius**2
+//console.log(results);
+
+let likes =11;
+likes =likes+1;
+
+console.log(likes);
