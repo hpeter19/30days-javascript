40d701919ae3e5da4533009d59955dd0dae0886b
hpeter19 <njoshpitar@gmail.com>
2025-05-08 12:05:12 +0300

    replacing a string

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -2,6 +2,7 @@ let email = 'njosh@gmail.com';
 //let result = email.lastIndexOf('n');
 
 
-let result = email.slice(0,6);
+//let result = email.slice(0,6);
 
+let result = email.replace('n','m');
 console.log(result);
