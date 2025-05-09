072c7155192e736e34d4530ca561ed8181a910df
hpeter19 <njoshpitar@gmail.com>
2025-05-09 10:21:58 +0300

    understanding not a nummber

--- chapter1/sandbox.js
+++ chapter1/sandbox.js
@@ -7,6 +7,13 @@ const pi= 3.14;
 //console.log(results);
 
 let likes =11;
-likes =likes+1;
+//likes =likes+1;
+//likes ++;
+//likes --;
+//likes += 10
+//likes -= 5
+//likes *= 2
+//console.log(likes);
+// NaN Not a number
 
-console.log(likes);
+console.log(5/'hello');
