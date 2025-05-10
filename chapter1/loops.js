d1220e721487702a3ffd897413b98aa1d010eecd
hpeter19 <njoshpitar@gmail.com>
2025-05-10 23:42:25 +0300

    loop using while

--- chapter1/loops.js
+++ chapter1/loops.js
@@ -6,6 +6,20 @@
 console.log('loop finished'); */
 
 const names = ["sam","Mike","olivia"];
- for (let i =0; i<names.length; i++){
+ /*for (let i =0; i<names.length; i++){
+  //console.log(names[i]);
+  let html=` <div>${names[i]}</div>`;
+   console.log(html);
+ } 
+let i =0;
+ while (i<5){
+     console.log('in loop:',i);
+     i++;
+ } */
+
+let i =0;
+while(i<names.length){
   console.log(names[i]);
- }
+  i++;
+} 
+
