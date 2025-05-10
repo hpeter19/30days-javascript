40606a9e50ad0f48ea51a1c22b5a6ecbd10e0c54
hpeter19 <njoshpitar@gmail.com>
2025-05-10 12:33:44 +0300

    iterating the arrray

--- chapter1/loops.js
+++ chapter1/loops.js
@@ -1,6 +1,11 @@
 // for loops
-for(let i=0; i<5; i++){
+/* for(let i=0; i<5; i++){
   console.log('in loop:',i);
 }
 
-console.log('loop finished');
+console.log('loop finished'); */
+
+const names = ["sam","Mike","olivia"];
+ for (let i =0; i<names.length; i++){
+  console.log(names[i]);
+ }
