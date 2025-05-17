bd89928caa0a79b5bafa18c7f8328ed6223e48b1
hpeter19 <njoshpitar@gmail.com>
2025-05-17 22:24:02 +0300

    adding break keyword to avoid printing other nessages

--- chapter1/loops.js
+++ chapter1/loops.js
@@ -91,16 +91,22 @@ const grade= 'D';
 switch (grade){
   case 'A':
     console.log('you got an A');
+    break;
   case 'B':
       console.log('you got an B');
+      break;
   case 'C':
         console.log('you got an C');
+        break;
   case 'D':
     console.log('you got an D');
+    break;
   case 'E':
     console.log('you got an E');
+    break;
   case 'F':
-    console.log('you got an F'); 
+    console.log('you got an F');
+    break; 
   default:
     console.log('Not a vslid grade');    
 }
