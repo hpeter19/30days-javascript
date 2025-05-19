f4585d5c0897fd96e18b227b50f5f10a78314018
hpeter19 <njoshpitar@gmail.com>
2025-05-19 13:21:17 +0300

    array method to filter items based on conditions

--- chapter1/functions.js
+++ chapter1/functions.js
@@ -57,3 +57,14 @@ console.log(`${index} -hello ${person}`);
      console.log(index,person);
 });*/
 people.forEach(logperson);
+
+//arrays 
+
+function isOdd(num) {
+    return num % 2 !== 0;
+  }
+  const arr = [1, 2, 3, 4, 5];
+  const oddNums = arr.filter(isOdd);
+  console.log(oddNums); // Outputs [1, 3, 5];
+  console.log(arr); // Outputs [1, 2, 3, 4, 5], original array is not affected
+  
