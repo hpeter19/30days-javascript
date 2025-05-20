04d3f4b08fa229eb76b03c4d3e6277489b473d33
hpeter19 <njoshpitar@gmail.com>
2025-05-20 16:29:31 +0300

    adding inline styles

--- chapter1/selectors.js
+++ chapter1/selectors.js
@@ -11,3 +11,13 @@ const controls = document.querySelector(".controls");
 // selects the prior sibling => .display
 const displays = controls.previousElementSibling;
 console.log(displays); // <div class="display"></div>
+
+// adds the indicated style rule to the element in the div variable
+div.style.color = "blue";
+
+// adds several style rules
+div.style.cssText = "color: blue; background: white;";
+
+// adds several style rules
+div.setAttribute("style", "color: blue; background: white;");
+
