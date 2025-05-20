34d65251d2f1e290ecc9e7ea960540c2c63224fc
hpeter19 <njoshpitar@gmail.com>
2025-05-20 17:45:42 +0300

    iterating through each event listners

--- chapter1/selectors2.js
+++ chapter1/selectors2.js
@@ -11,3 +11,24 @@ container.appendChild(content);
 const btn = document.querySelector("#btn");
 btn.onclick = () => alert("Hello World");
 
+/*btn.addEventListener("click", function (e) {
+    console.log(e.target);
+  }); */
+
+  btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+  });
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+
+  
