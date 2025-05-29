ae132fcf97cf6d1f72ef97ff38d49b6e8216c396
hpeter19 <njoshpitar@gmail.com>
2025-05-29 18:44:58 +0300

    adding methods to objects

--- chapter5/sandbox.js
+++ chapter5/sandbox.js
@@ -4,18 +4,11 @@ let user = {
    age: 30,
    email: 'maloo@gmail.com',
    location: 'berlin',
-   blogs: ['why cheese and mark rules','10things to make with marmite']
+   blogs: ['why cheese and mark rules','10things to make with marmite'],
+   login: function(){
+    console.log('the user logged in');
+   }
    };
 
-console.log(user);
-console.log(user.name); 
 
-user.age= 35;
-
-console.log(user.age);
-
-console.log(user['name']);
-user['name']='mathew';
-
-console.log(user.name);
-console.log(typeof user);
+   user.login();
