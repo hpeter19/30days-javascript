b5fba79a1f93afffefbf72dba5f90ce082791b52
hpeter19 <njoshpitar@gmail.com>
2025-05-29 13:02:32 +0300

    creating an object

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+
+let user = {
+   name: 'christine',
+   age: 30,
+   email: 'maloo@gmail.com',
+   location: 'berlin',
+   blogs: ['why cheese and mark rules','10things to make with marmite']
+   };
