c805b1b7254c3df318382b81242ad0e21bebd7a5
hpeter19 <njoshpitar@gmail.com>
2026-01-04 12:09:27 +0300

    styling the text

--- react-projects-2/src/Card.jsx
+++ react-projects-2/src/Card.jsx
@@ -4,8 +4,8 @@ function Card(){
   return(
     <div className="card" >
        <img className='card-image' src={profilePic} alt="profile picture" />
-       <h2>Pacific</h2>
-       <p>I Study Chemistry and read Books</p>
+       <h2 className='card-title'>Pacific</h2>
+       <p className='card-text'>I Study Chemistry and read Books</p>
     </div>
   );
 }
