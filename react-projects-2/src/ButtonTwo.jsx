092167f473fcb6cc232866a9a0ca397c5d8f17ff
hpeter19 <njoshpitar@gmail.com>
2026-01-08 14:37:43 +0300

    function to set quantity

--- react-projects-2/src/ButtonTwo.jsx
+++ react-projects-2/src/ButtonTwo.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 function ButtonTwo(){
 
-    const [name,setName] =useState("")
-    const [quantity, setQuantity]= useState("")
+    const [name,setName] =useState("");
+    const [quantity, setQuantity]= useState();
 
     function handleNameChange(event){
         setName(event.target.value)
+    }
+
+    function handleQuantityChange(){
+        setQuantity(event.target.value);
     }
 
     return(
         <div>
             <input value={name} onChange={handleNameChange} />
             <p>Name:{name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type="number" />
+            <p>Quantity: {quantity}</p>
         </div>
     );
 }
