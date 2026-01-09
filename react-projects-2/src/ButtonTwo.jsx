45031d861506be511acc99203f9b5f65f5f53e3c
hpeter19 <njoshpitar@gmail.com>
2026-01-09 22:15:18 +0300

    radio buttons

--- react-projects-2/src/ButtonTwo.jsx
+++ react-projects-2/src/ButtonTwo.jsx
@@ -1,47 +1,75 @@
 import { useState } from "react";
-function ButtonTwo(){
+function ButtonTwo() {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState();
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
 
-    const [name,setName] =useState("");
-    const [quantity, setQuantity]= useState();
-    const [comment, setComment] =useState("");
-    const [payment, setPayment] = useState("");
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
 
-    function handleNameChange(event){
-        setName(event.target.value)
-    }
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
 
-    function handleQuantityChange(event){
-        setQuantity(event.target.value);
-    }
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
 
-    function handleCommentChange(event){
-        setComment(event.target.value)
-    }
+  function handlePaymentChange(event) {
+    setPayment(event.target.value);
+  }
 
-    function handlePaymentChange(event){
-         setPayment(event.target.value)
-    }
+  function handleShippingChange(event) {
+    setShipping(event.target.value);
+  }
 
-    return(
-        <div>
-            <input value={name} onChange={handleNameChange} />
-            <p>Name:{name}</p>
+  return (
+    <div>
+      <input value={name} onChange={handleNameChange} />
+      <p>Name:{name}</p>
 
-            <input value={quantity} onChange={handleQuantityChange} type="number" />
-            <p>Quantity: {quantity}</p>
+      <input value={quantity} onChange={handleQuantityChange} type="number" />
+      <p>Quantity: {quantity}</p>
 
-            <textarea value={comment} onChange={handleCommentChange}
-             placeholder="Enter delivery options" />
-             <p>comment: {comment}</p>
+      <textarea
+        value={comment}
+        onChange={handleCommentChange}
+        placeholder="Enter delivery options"
+      />
+      <p>comment: {comment}</p>
 
-             <select value={payment} onChange={handlePaymentChange}>
-               <option value="">select an option</option>
-               <option value="Visa">Visa</option>
-               <option value="Mastercard">MasterCard</option>
-               <option value="GiftCard">GiftCard</option>
-             </select>
-              <p>Payment:{payment}</p>
-        </div>
-    );
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">MasterCard</option>
+        <option value="GiftCard">GiftCard</option>
+      </select>
+      <p>Payment:{payment}</p>
+
+      <label>
+        <input
+          type="radio"
+          value="pick up"
+          checked={shipping === "pick up"}
+          onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="Delivery"
+          checked={shipping === "Delivery"}
+          onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>
+      <p>Shipping: {shipping}</p>
+    </div>
+  );
 }
-export default ButtonTwo
+export default ButtonTwo;
